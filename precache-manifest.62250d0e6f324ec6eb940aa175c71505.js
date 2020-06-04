45ff713825e49b74774ae17fb5417ea627a9143c
Вася Иванов <soldatik2005@mail.ru>
2020-06-04 22:55:04 +0300

    Updates

---
+++ precache-manifest.62250d0e6f324ec6eb940aa175c71505.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1bcb5e368b8aab7317ce0baa78443e2",
+    "url": "/sendsay/index.html"
+  },
+  {
+    "revision": "b895303c95f27441a085",
+    "url": "/sendsay/static/css/main.a38bcdb6.chunk.css"
+  },
+  {
+    "revision": "34b726d5a5f0c389daee",
+    "url": "/sendsay/static/js/2.17e51bd0.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/sendsay/static/js/2.17e51bd0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b895303c95f27441a085",
+    "url": "/sendsay/static/js/main.37c0cdc8.chunk.js"
+  },
+  {
+    "revision": "132ba77b9d04e9cfd75c",
+    "url": "/sendsay/static/js/runtime-main.01231fd9.js"
+  },
+  {
+    "revision": "404e4373cba1344d28a4a257152ac8b8",
+    "url": "/sendsay/static/media/SF-Pro-Text-Regular.404e4373.otf"
+  },
+  {
+    "revision": "7cf44a952fa9b68dfc7887836677598d",
+    "url": "/sendsay/static/media/paperclip.7cf44a95.svg"
+  },
+  {
+    "revision": "f81c783407776b53bfd34f2bfa6af29b",
+    "url": "/sendsay/static/media/trash.f81c7834.svg"
+  }
+]);
